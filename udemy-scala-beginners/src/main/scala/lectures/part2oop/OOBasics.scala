6ba2c5e140baf52ae298b30cd4bab66b2d20ddbb
kshashank03 <shashankkalanithi@gmail.com>
2022-09-19 19:35:05 +0900

    OOP practice

--- udemy-scala-beginners/src/main/scala/lectures/part2oop/OOBasics.scala
+++ udemy-scala-beginners/src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,5 +1,7 @@
 package lectures.part2oop
 
+import java.io.Writer
+
 object OOBasics extends App {
 
   val person = new Person("John", 12) // Instantiating a class
@@ -9,6 +11,11 @@ object OOBasics extends App {
   println(person.x)
   println(person.greet("Daniel"))
 
+  // Exercises
+
+  // Instantiate a writer
+  val locke = TheWriter("John", "Locke", 1600) // "Writer" is a Java class, had to rename
+  println(locke.fullName())
 }
 
 // Writing a class
@@ -32,3 +39,39 @@ class Person(name: String, val age:Int) { // Classes organize data and behavior
 }
 // Age is a class parameter but not a class member
 // You can add "val" or "var" prior to a class parameter to make it a field
+
+// Create a Novel and Writer class
+  // Writer: firstname, lastname, year
+     // Method fullname
+
+class Novel(){
+
+}
+
+class TheWriter(firstName:String, lastName:String, val bornYear: Int){
+  def fullName(firstName:String = firstName, lastName:String = lastName): String = firstName + " " + lastName
+  // val age:Int = bornYear
+  // Method: fullname
+
+}
+
+class TheNovel(name:String, yearRel:Int, author:TheWriter){
+  val authorAge = this.yearRel - this.author.bornYear
+  val isWrittenBy = author != None
+  def copy(newYear:Int): TheNovel = TheNovel(this.name, newYear, this.author)
+}
+
+class TheCounter(start:Int){
+
+}
+
+  // Novel: name, year of release, author -> Instance of type Writer
+    // - authorAge -> Age of author at year of release
+    // - isWrittenBy(author)
+    // - copy (new year of release) = returns new instance of Novel with new year of release
+
+// Counter class
+  // Receives int
+  // method current count
+  // method to increment/decrement => new counter
+  // overload inc/dec to receive an amount
