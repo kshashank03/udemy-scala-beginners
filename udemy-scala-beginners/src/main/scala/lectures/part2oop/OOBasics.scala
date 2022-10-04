b1a54b7b75db24f07e04df70c673ef640fc84638
kshashank03 <shashankkalanithi@gmail.com>
2022-10-04 16:43:17 -0700

    oop additions

--- udemy-scala-beginners/src/main/scala/lectures/part2oop/OOBasics.scala
+++ udemy-scala-beginners/src/main/scala/lectures/part2oop/OOBasics.scala
@@ -44,21 +44,20 @@ class Person(name: String, val age:Int) { // Classes organize data and behavior
   // Writer: firstname, lastname, year
      // Method fullname
 
-class Novel(){
-
-}
 
 class TheWriter(firstName:String, lastName:String, val bornYear: Int){
-  def fullName(firstName:String = firstName, lastName:String = lastName): String = firstName + " " + lastName
+//  Below was my method
+//  def fullName(firstName:String = firstName, lastName:String = lastName): String = firstName + " " + lastName
+    def fullName: String = firstName + " " + lastName // This is Daniel's method
   // val age:Int = bornYear
   // Method: fullname
 
 }
 
 class TheNovel(name:String, yearRel:Int, author:TheWriter){
-  val authorAge = this.yearRel - this.author.bornYear
+  def authorAge: Int = this.yearRel - this.author.bornYear
   val isWrittenBy = author != None
-  def copy(newYear:Int): TheNovel = TheNovel(this.name, newYear, this.author)
+  def copy(newYear:Int): TheNovel = new TheNovel(this.name, newYear, this.author)
 }
 
 // Novel: name, year of release, author -> Instance of type Writer
@@ -66,13 +65,16 @@ class TheNovel(name:String, yearRel:Int, author:TheWriter){
   // - isWrittenBy(author)
   // - copy (new year of release) = returns new instance of Novel with new year of release
 
+
+// The below is a "getter" and is the same as creating a class then defining a method to "Get" the count
+//class Counter(val count: Int){
+//
+//}
 class TheCounter(start:Int){
   def currentCount(): Int = this.start
-  def crement(value:Int, incDec:String): TheCounter = {
-    if(incDec == "inc") TheCounter(value + 1) else TheCounter(value - 1)
-  }
-
-}
+  def inc: Int = new TheCounter(count + 1) // Creates a new instance which allows us to "modify" the "count" val
+  def dec: Int = new TheCounter(count - 1)
+ }
 
 // Counter class
   // Receives int
